74ec88093c95dee5136a9b4759af9c14abbc6cd0
Leonardo Fontana <tetsuo2501@gmail.com>
2015-10-21 18:38:13 +0200

    Singleton Volley and image resize&cache

--- app/src/main/java/info/leonardofontana/modemme/MainActivity.java
+++ app/src/main/java/info/leonardofontana/modemme/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import info.leonardofontana.modemme.util.VolleyRequestQueue;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,14 +20,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //////Inizializzo la coda dei download con cache
+        VolleyRequestQueue.getInstance(getApplicationContext());
+
+
     }
 
     @Override
--- app/src/main/java/info/leonardofontana/modemme/util/VolleyRequestQueue.java
+++ app/src/main/java/info/leonardofontana/modemme/util/VolleyRequestQueue.java
@@ -1,12 +1,100 @@
 package info.leonardofontana.modemme.util;
 
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
 
 /**
  * Created by tetsu on 19/10/2015.
  */
 public class VolleyRequestQueue {
+    private static VolleyRequestQueue mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
+    protected final int IMAGE_WIDTH = 800;
+    protected final int MAX_HEIGHT = 450;
+
+    private VolleyRequestQueue(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(
+                                url,
+                                filterImage(bitmap)
+                        );
+                    }
+
+                    private Bitmap filterImage(Bitmap in){
+                        Matrix transform = new Matrix();
+                        float scala = in.getWidth()/IMAGE_WIDTH;
+                        transform.preScale(scala,scala);
+                        transform.postTranslate(
+                                0f,
+                                ((scala - 1) * in.getHeight()) / 2f
+                        );
+                        Paint paint = new Paint();
+                        paint.setFilterBitmap(true);
+                        Bitmap out = Bitmap.createBitmap(
+                                IMAGE_WIDTH,
+                                (int) (in.getHeight()*scala),
+                                Bitmap.Config.ARGB_8888);
+                        Canvas canvas = new Canvas(out);
+                        canvas.drawBitmap(in, transform, paint);
+                        return (out.getHeight() > MAX_HEIGHT) ?
+                                //todo: Settare i valori per centrare l'immagine
+                                Bitmap.createBitmap(out, 0, 0, IMAGE_WIDTH, MAX_HEIGHT):
+                                out;
+
+                    }
+                });
+    }
 
 
+
+    public static synchronized VolleyRequestQueue getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleyRequestQueue(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
 
 }
